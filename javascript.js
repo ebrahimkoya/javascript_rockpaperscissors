32fd9b613247d584d55973cbaca474197ef3b328
ebrahimkoya <koyaebrahim@gmail.com>
2026-02-26 06:37:37 -0500

    gui mostly done

--- javascript.js
+++ javascript.js
@@ -32,7 +32,7 @@ function playRound(getComputerChoice,getHumanChoice) {
         if (getHumanChoice == "paper") {
             return "tie"
         }
-        if (getHumanChoice == "scissor") {
+        if (getHumanChoice == "scissors") {
             return "you win!!"
         }
         if (getHumanChoice == "rock") {
@@ -52,28 +52,46 @@ function playRound(getComputerChoice,getHumanChoice) {
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let roundCount = 0;
-    while (roundCount < 5) {
-        x = playRound(getComputerChoice(), getHumanChoice())
-        if (x == "you win!!"){
-            humanScore += 1
-        }
-        if (x == "you lose!!"){
-            computerScore += 1
-        }
-        roundCount += 1
-        console.log(x)
-        console.log(roundCount)
-        console.log(humanScore, computerScore)
-    }
-    if (humanScore > computerScore) {
-        return humanScore
-    }
-    else {
-        return computerScore
-    }
-}
-playGame();
+const btn_rock = document.querySelector('#rock');
+const btn_paper = document.querySelector('#paper');
+const btn_scissors = document.querySelector('#scissors');
+
+
+btn_rock.addEventListener('click', () => {
+    const comp_choice = getComputerChoice();
+    const result = (playRound(comp_choice,'rock'));
+    console.log(humanScore, computerScore, result, comp_choice)
+    if (result == "you win!!") humanScore ++
+    if (result == "you lose!!") computerScore ++
+    score_board.textContent = result + ' The score is: Human = ' + humanScore +' and Computer = '+ computerScore;
+
+})
+
+btn_paper.addEventListener('click', () => {
+    const comp_choice = getComputerChoice();
+    const result = (playRound(comp_choice,'paper'));
+
+    if (result == "you win!!") humanScore ++
+    if (result == "you lose!!") computerScore ++
+    score_board.textContent = result + ' The score is: Human = ' + humanScore +' and Computer = '+ computerScore;
+
+})
+
+btn_scissors.addEventListener('click', () => {
+    const comp_choice = getComputerChoice();
+    const result = (playRound(comp_choice,'scissors'));
+
+    if (result == "you win!!") humanScore ++
+    if (result == "you lose!!") computerScore ++
+    score_board.textContent = result + ' The score is: Human = ' + humanScore +' and Computer = '+ computerScore;
+
+})
+let humanScore = 0;
+let computerScore = 0;
+
+const score_display = document.querySelector("#score_display");
+const score_board = document.createElement("p");
+score_board.classList.add("score_board");
+score_display.appendChild(score_board);
+score_board.style.fontSize = 'x-large';
+score_board.textContent = "Click a choice to play!"
